be9045574f1cc3d96a798aa9a575b4c8219983f7
ddrawer14 <ddrawer14@yahoo.com>
2015-11-18 02:55:25 +0000

    React and Flux

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,9 @@
 import './main.css';
 
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
@@ -7,6 +11,9 @@ import App from './components/app.jsx';
 main();
 
 function main() {
+  
+  persist(alt, storage, 'app');
+
   const app = document.createElement('div');
 
   document.body.appendChild(app);
